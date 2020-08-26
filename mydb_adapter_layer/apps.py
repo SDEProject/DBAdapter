bcfb30c9bac2637f62a47ab55fd237b0d88651aa
Valentina Odorizzi <valentina.odorizzi@studenti.unitn.it>
2020-08-26 17:28:45 +0200

    Updload adapter project

---
+++ mydb_adapter_layer/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MydbBusinessLogicLayerConfig(AppConfig):
+    name = 'mydb_adapter_layer'
---
+++ mydb_adapter_layer/run.sh
@@ -0,0 +1,2 @@
+echo "Starting mydb_adapter_layer service..."
+TRAVELANDO_SERVICE=mydb_adapter_layer python manage.py runserver 0.0.0.0:50005
